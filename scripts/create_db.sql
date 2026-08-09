389b8a413338b073e93909cd485b7d587f2eaff3
mahmoudhussein1227 <mahmoudhussein1227@gmail.com>
2026-08-09 21:54:47 +0300

    add comment header

--- scripts/create_db.sql
+++ scripts/create_db.sql
@@ -1,4 +1,7 @@
--- lets create schemas for the datawarehouse arch. different stages
+-- Author: Mahmoud Hussein
+-- Purpose: Create the DataWarehouse database and initialize the bronze, silver, and gold schemas
+-- for a staged data warehouse architecture.
+
 use master ;
 IF EXISTS (SELECT 1 from sys.databases WHERE name = 'DataWarehouse')
 BEGIN
